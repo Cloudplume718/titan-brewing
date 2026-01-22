22d131bd4894859d3476360c98f0ebff61f912ea
Yang qiou <1332394192@qq.com>
2026-01-22 23:07:14 +0800

    Remove final keystatic legacy code

--- app/keystatic/[[...params]]/page.tsx
+++
@@ -1,5 +0,0 @@
-import KeystaticApp from '../keystatic'; // 🟢 引用上一级目录的 keystatic.tsx
-
-export default function Page() {
-  return null;
-}
--- lib/reader.ts
+++
@@ -1,5 +0,0 @@
-import { createReader } from '@keystatic/core/reader';
-import config from '@/keystatic.config';
-
-// 🟢 创建一个读取器，它会自动根据配置文件去硬盘里找数据
-export const reader = createReader(process.cwd(), config);
